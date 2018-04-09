34b8e147e82f4c796fa8cd8befc15ead804de33f
Aapo Komppa <aapo.komppa@pp.inet.fi>
2018-04-09 13:24:54 +0300

    index.js fix

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-require('./daterangepicker.js');
-require('./angular-daterangepicker.js');
+require('./daterangepicker++.js');
+require('./angular-daterangepicker++.js');
 module.exports = 'angular-hasinline-daterangepicker'
